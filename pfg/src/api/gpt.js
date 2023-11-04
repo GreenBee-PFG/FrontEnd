e576a5ba89a4a86dd1b4c78253f8961a722d2a9b
Hamtoto <gohg0901@naver.com>
2023-11-04 22:14:17 +0900

    ✨ 로딩 구현 및 직무 입력란 생성, api 호출 스크립트 수정

--- pfg/src/api/gpt.js
+++ pfg/src/api/gpt.js
@@ -1,24 +1,38 @@
-export const CallGPT = async () => {
-    //console.log(">>CallGPT");
-    /*
-    curl https://api.openai.com/v1/chat/completions \
-  -H "Content-Type: application/json" \
-  -H "Authorization: Bearer $OPENAI_API_KEY" \
-  -d '{
-     "model": "gpt-3.5-turbo",
-     "messages": [{"role": "user", "content": "Say this is a test!"}],
-     "temperature": 0.7
-   }'
-    */
-   const response = await fetch("https://api.openai.com/v1/chat/completions",{
+export const CallGPT = async ({ prompt }) => {
+/**
+For output, use the following JSON format:
+{
+question: [Your question should be entered here.]
+}
+  
+ */
+    const messages = [
+        {
+            "role": "system",
+            "content": "You are a hiring interviewer. When a user enters a job, interview questions tailored to that job can be created. Please only ask one interview question and do not print out examples."
+        },
+        {
+            "role": "user",
+            "content": `Translate into Korean and use the output in the following JSON format:
+            {
+                response: [Your question should be entered here.]
+            }`
+        },
+        {
+            "role": "user",
+            "content": `${prompt}`
+        }
+    ];
+
+    const response = await fetch("https://api.openai.com/v1/chat/completions",{
     method:"POST",
     headers:{
         "Content-Type": "application/json",
-        "Authorization": `Bearer ${process.env.REACT_APP_CHAT_GPT_API_KEY}`,
+        "Authorization": `Bearer ${process.env.REACT_APP_CHAT_GPT_API_KEY}`
     },
     body:JSON.stringify({
         "model": "gpt-3.5-turbo",
-        "messages": [{"role": "user", "content": "Hello World!"}],
+        messages,
         "temperature": 0.7,
         "max_tokens": 1_000,
     }),
--- pfg/src/pages/DevApi.jsx
+++ pfg/src/pages/DevApi.jsx
@@ -2,22 +2,36 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import { CallGPT } from "../api/gpt"
 
+import Spinner from "../component/Spinner";
 import Button from "../component/Button";
 
 const BodyContent = styled.div`
-  width: 100%;
+  width: 80%;
   display: flex;
   flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+const Textarea = styled.textarea`
+  text-align: center;
+  width: 180px;
+  height: 25px;
+  resize: none;
+  font-size: 16px;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
 `
 
 const DevApi = () => {
     const [data, setData] = useState("");
     const [isLoading, setIsLoading] = useState(false);
+    const [content, setContent] = useState("");
 
     const handleClickAPICall = async() => {
         try {
             setIsLoading(true);
-            const message = await CallGPT();
+            const message = await CallGPT({prompt: content});
             setData(message);
         } catch (e){
             console.error(e)
@@ -26,13 +40,24 @@ const DevApi = () => {
         }
     };
 
+    const handleContentChange = (e) => {
+        setContent(e.target.value);
+      };
+
     return (
         <>
             <BodyContent>
                 <h1>dev gpt api</h1>
+
                 <Button onClick={() => handleClickAPICall()}>GPT API call</Button>
-                <div>data : {data}</div>
-                <div>isLoading : {isLoading ? "loading.." : "fin"}</div>
+                <div>{data}</div>
+                <div>isLoading : {isLoading ?  <Spinner /> : "fin"}</div>
+                <Textarea
+                    id="textarea_content"
+                    placeholder="직무를 입력해주세요"
+                    onChange={handleContentChange}
+                    value={content}
+                />
             </BodyContent>
         </>
     );
