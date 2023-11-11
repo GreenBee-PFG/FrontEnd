ae3997130ea7154856bd36f09ddde6ed20dc0c9a
Hamtoto <gohg0901@naver.com>
2023-11-11 19:51:41 +0900

    🔥 미사용 코드 제거

--- pfg/src/component/Header.jsx
+++ pfg/src/component/Header.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import styled from "styled-components";
-import HeaderItem from "./Header-NavItem";
 import config from "../config";
 
 /* gray #343439 */
@@ -15,13 +14,13 @@ const StyledHeader = styled.div`
   height: ${config.HeaderHeight};
   width: 100%;
   z-index: 9999; /* 필요에 따라 적절한 z-index 값을 설정하세요 */
-`;
+`
 
 const Menu = styled.div`
   display: flex;
   flex-direction: row;
   margin-left: auto;
-`;
+`
 
 const Headerdiv = styled.div`
   display: flex;
@@ -29,41 +28,26 @@ const Headerdiv = styled.div`
   height: 100%;
   max-width: ${config.BodyWidth};
   margin: 0 auto;
-`;
+`
 
 const Logo = styled.h1`
   color: white;
   text-decoration: none;
   margin: auto;
   margin-left : 20px;
-`;
+`
 
 function Header() {
-  const menus = [
-  ];
 
   return (
     <StyledHeader>
       <Headerdiv>
         <Logo>
           <NavLink to="/" style={{ color: "white", textDecoration: "none" }}>
-            LOGO
+            PFG
           </NavLink>
         </Logo>
         <Menu>
-          {menus.map((menu, index) => {
-            return (
-              <NavLink
-                exact
-                style={{ color: "white", textDecoration: "none", margin: "auto 20px auto 0" }}
-                to={menu.path}
-                key={index}
-                activeStyle={{ color: "black" }}
-              >
-                <HeaderItem menu={menu} />
-              </NavLink>
-            );
-          })}
         </Menu>
       </Headerdiv>
     </StyledHeader>
