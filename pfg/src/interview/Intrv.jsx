c31493af906166fb7e17a1d2bbb3fd5cf7125d53
aicul313 <lucia_0313@naver.com>
2023-11-15 01:02:50 +0900

    💄 Intrv 페이지 UI 업데이트

--- pfg/src/interview/Intrv.jsx
+++ pfg/src/interview/Intrv.jsx
@@ -9,9 +9,14 @@ const BodyContent = styled.div`
   justify-content: center;
   align-items: center;
 `
+
+const OccupationField = styled.div`
+  display: flex;
+`
+
 const Textarea = styled.textarea`
   text-align: center;
-  width: 240px;
+  width: 200px;
   height: 25px;
   resize: none;
   font-size: 16px;
@@ -20,6 +25,7 @@ const Textarea = styled.textarea`
   border-radius: 5px;
 `
 const Button = styled.button`
+  margin-left: 10px;
   padding: 10px 20px;
   background-color: #f0f0f0;
   color: 333;
@@ -32,6 +38,42 @@ const Button = styled.button`
     color: white;
     background-color: #555;
   }
+`
+
+const Hidden = styled.h1`
+  clip: rect(1px, 1px, 1px, 1px);
+  clip-path: inset(50%);
+  width: 1px;
+  height: 1px;
+  margin: -1px;
+  overflow: hidden;
+  padding: 0;
+  position: absolute;
+`
+
+const AiWrapper = styled.div`
+  margin-bottom: 20px;
+  position: relative;
+
+  &:after {
+    content: "AI";
+      color: #fff;
+      background-color: #343439;
+      width: 30px;
+      height: 30px;
+      text-align: center;
+      line-height: 27px;
+      border-radius: 50px;
+      position: absolute;
+      top: 22px;
+      left: -16%;
+  }
+`
+
+const AiText = styled.p`
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
 `
 
 const Intrv = () => {
@@ -55,21 +97,26 @@ const Intrv = () => {
     return (
         <>
             <BodyContent>
-                <h1>A.I 면접 질문 생성기</h1>
-                <Textarea
-                    id="textarea_content"
-                    placeholder="직무를 입력해주세요"
-                    onChange={handleJobChange}
-                    onKeyPress={(event) => {
-                        if (event.key === 'Enter') {
-                            event.preventDefault();
-                        }
-                    }}
-                    maxLength={14}
-                    value={job}
-                />
-                <br />
-                <Button onClick={() => handleClickPlay()}>면접 보기</Button>
+                <Hidden>A.I 면접 질문 생성기</Hidden>
+                <AiWrapper>
+                  <AiText>당신의 직무를 입력해주세요</AiText>
+                </AiWrapper>
+                <OccupationField>
+                  <Textarea
+                      id="textarea_content"
+                      placeholder="직무를 입력해주세요"
+                      onChange={handleJobChange}
+                      onKeyPress={(event) => {
+                          if (event.key === 'Enter') {
+                              event.preventDefault();
+                          }
+                      }}
+                      maxLength={14}
+                      value={job}
+                  />
+                  <br />
+                  <Button onClick={() => handleClickPlay()}>면접 보기</Button>
+                </OccupationField>
             </BodyContent>
         </>
     );
